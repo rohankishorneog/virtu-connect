7368e4c71550652a5583a5e00ed9cd96bb098cd0
rohankishorneog <rkn2601@gmail.com>
2024-10-30 12:55:53 +0530

    fixed orientation of sign in

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="w-screen flex justify-center h-screen  items-center">
+      <SignIn />
+    </div>
+  );
 }
